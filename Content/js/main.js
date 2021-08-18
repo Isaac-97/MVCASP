95b108c959779f7afc0f4b38f06aca50f6a89265
alexander <alexander_mouse@outlook.com>
2021-08-18 16:54:51 -0500

    Cambios en layout, se agrego css y js

---
+++ Content/js/main.js
@@ -0,0 +1,21 @@
+﻿
+var openwindows = document.getElementById('openwindows');
+openwindows.addEventListener('click', function () {
+ let submenu = document.getElementById('submenu');
+ let headarriba = document.getElementById('headarriba');
+ submenu.classList.remove("cerrar");
+ submenu.classList.add("abrir");
+ headarriba.style.width="78%";
+ headarriba.style.right="0%"
+});
+
+
+var closewindow = document.getElementById('closewindow');
+closewindow.addEventListener('click', function () {
+ let submenu = document.getElementById('submenu');
+ let headarriba = document.getElementById('headarriba');
+ submenu.classList.add("cerrar");
+ submenu.classList.remove("abrir");
+ headarriba.style.width="100%";
+ headarriba.style.transitionDuration="0.5s";
+});
