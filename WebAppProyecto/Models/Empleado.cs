6801a69b3dae2a5fe1ccefeffc30d769173d42d6
alexander <alexander_mouse@outlook.com>
2021-09-04 12:03:43 -0500

    Movi en Modelo V de empleados

--- WebAppProyecto/Models/Empleado.cs
+++ WebAppProyecto/Models/Empleado.cs
@@ -9,16 +9,27 @@ namespace WebAppProyecto.Models
     public class Empleado
     {
         [Key]
+        [Display (Name = "CvEmpleado")]
         public int IdEmpleado { get; set; }
+        [Display (Name = "Nombre")]
         public string Nombre { get; set; }
+        [Display (Name = "Apellido Paterno")]
         public string APaterno { get; set; }
+        [Display (Name = "Apellido Materno")]
         public string AMaterno { get; set; }
+        [Display (Name = "Celular")]
         public string Telefono { get; set; }
         public DateTime FechaNacimiento { get; set; }
+        [Display(Name = "Fecha de Registro")]
         public DateTime FechaRegistro { get; set; }
+        [StringLength (18, MinimumLength = 18)]
+        [Display (Name = "Curp")]
         public string CURP { get; set; }
+        [Display (Name = "Tipo Sexo")]
         public int IdSexo { get; set; }
+        [Display(Name = "Direccion")]
         public string Direccion { get; set; }
+        [Display(Name = "Email")]
         public string Correo { get; set; }
     }
 }
